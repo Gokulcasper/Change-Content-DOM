cd490576a5d99fb70ee4450d7ecedcf75af06a28
Gokulcasper <ronitcasper215@gmail.com>
2022-06-17 17:38:06 +0530

    first commit

---
+++ script2.js
@@ -0,0 +1,15 @@
+// function createAlertViaDom(Msg) {
+//   const alertId = document.getElementById("alertId");
+
+//   // Creating Elements
+//   const div = document.createElement("div");
+
+//   div.className = "alert";
+
+//   const textNode = document.createTextNode(Msg);
+//   div.append(textNode);
+//   console.log(div);
+
+//   alertId.prepend(div);
+// }
+// createAlertViaDom("Successfully Updated!!");
